df4b939cd5d7586276fc30461b0e7b799e0d49b9
Ekaterina <davydovakaterina2022@gmail.com>
2022-12-02 11:41:31 +0300

    tasks

---
+++ task_2/script.js
@@ -0,0 +1,20 @@
+function sum() {
+        let a = Number(prompt('Введите первое число'));
+    let b = Number(prompt('Введите второе число'));
+    alert(a + b);
+    }
+    function sub() {
+        let a = prompt('Введите первое число');
+    let b = prompt('Введите второе число');
+    alert(a - b);
+    }
+    function mult() {
+        let a = prompt('Введите первое число');
+    let b = prompt('Введите второе число');
+    alert(a * b);
+    }
+    function div() {
+        let a = prompt('Введите первое число');
+    let b = prompt('Введите второе число');
+    alert(a / b);
+    }
